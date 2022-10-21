d8a4e04a75c5cba3bacca34ac996dfc1db0f67ba
Tom Gandolfo <tomgandolfo2@gmail.com>
2022-10-20 21:44:02 -0300

    Contact form continued

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -21,7 +21,8 @@ export default function Contact({}: Props) {
                     Thomas is the best.{" "}
                     <span className="underline decoration-[#F7AB0A]/50">Hire him!</span>
                 </h4>
-                <div>
+
+                <div className="flex flex-col space-y-10">
                     <div className="flex items-center space-x-5">
                         <PhoneIcon className="h-7 w-7 animate-pulse text-[#F7AB0A]" />
                         <p className="text-2xl">+123 4567 8909</p>
@@ -35,6 +36,22 @@ export default function Contact({}: Props) {
                         <p className="text-2xl">123 Example Street, Examplandia</p>
                     </div>
                 </div>
+
+                <form className="mx-auto flex w-fit flex-col space-y-2">
+                    <div className="flex space-x-2">
+                        <input placeholder="Name" className="contactInput" type="text" />
+                        <input placeholder="Email" className="contactInput" type="text" />
+                    </div>
+
+                    <input placeholder="Subject" className="contactInput" type="text" />
+                    <textarea placeholder="Message" className="contactInput" />
+                    <button
+                        type="submit"
+                        className="rounded-md bg-[#F7AB0A] py-5 px-10 text-lg font-bold text-black"
+                    >
+                        Submit
+                    </button>
+                </form>
             </div>
         </div>
     )
